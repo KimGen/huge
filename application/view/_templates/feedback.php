98a27a97ea41e5cb2eb0ad6b25b0d79f2e969296
Cristopher Castro <cristophernic@live.cl>
2018-11-15 18:32:52 -0300

    .

--- application/view/_templates/feedback.php
+++ application/view/_templates/feedback.php
@@ -7,13 +7,13 @@ $feedback_negative = Session::get('feedback_negative');
 // echo out positive messages
 if (isset($feedback_positive)) {
     foreach ($feedback_positive as $feedback) {
-        echo '<div class="feedback success">'.$feedback.'</div>';
+        echo '<div class="alert alert-info alert-dismissible fade show" role="alert">'.$feedback.'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
     }
 }
 
 // echo out negative messages
 if (isset($feedback_negative)) {
     foreach ($feedback_negative as $feedback) {
-        echo '<div class="feedback error">'.$feedback.'</div>';
+        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">'.$feedback.'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
     }
 }
