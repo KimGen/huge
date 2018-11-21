16d1d9273fb1a1f604286284a518fa31df38d9a6
Cristopher Castro <cristophernic@live.cl>
2018-11-21 11:23:17 -0300

    Mejoras de interface

--- application/view/ecografia/index.php
+++ application/view/ecografia/index.php
@@ -3,28 +3,31 @@
     <div class="row">
         <div class="col">
             <div class="card mb-3 mt-3">
-                <div class="card-block p-2">
+                <img class="card-img-top" alt="100%x180" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16736a1722b%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16736a1722b%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22101.5%22%20y%3D%2296.9%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                <div class="card-body">
                     <h6 class="card-title">Ecografía Obstétrica 1° Trimestre</h6>
-                    <p>Determinación de edad gestacional</p>
-                    <a href="#ecoObsPrimTrim" class="float-right">Ir a exámen</a>
+                    <p class="card-text">Determinación de edad gestacional</p>
+                    <a href="#ecoObsPrimTrim" class="btn btn-primary">Ir a exámen</a>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card mb-3">
-                <div class="card-block p-2">
+                <img class="card-img-top" alt="100%x180" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16736a1722b%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16736a1722b%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22101.5%22%20y%3D%2296.9%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                <div class="card-body">
                     <h6 class="card-title">Ecografía Obstétrica 2° - 3° Trimestre</h6>
-                    <p>Valoración del crecimiento intrauterino</p>
-                    <a href="#ecoObsSegTrim" class="card-link float-right">Ir a exámen</a>
+                    <p class="card-text">Valoración del crecimiento intrauterino</p>
+                    <a href="#ecoObsSegTrim" class="btn btn-primary">Ir a exámen</a>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card mb-3">
-                <div class="card-block p-2">
+            <img class="card-img-top" data-src="holder.js/100px180/" alt="100%x180" style="height: 180px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16736a1722b%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16736a1722b%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22101.5%22%20y%3D%2296.9%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                <div class="card-body">
                     <h6 class="card-title">Flujometría Doppler Materna y/o Fetal</h6>
-                    <p>Complemento a la biometría de crecimiento</p>
-                    <a href="#ecoDoppler" class="card-link float-right">Ir a exámen</a>
+                    <p class="card-text">Complemento a la biometría de crecimiento</p>
+                    <a href="#ecoDoppler" class="btn btn-primary">Ir a exámen</a>
                 </div>
             </div>
         </div>
