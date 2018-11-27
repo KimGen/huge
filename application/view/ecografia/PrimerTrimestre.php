ae46a3fe156e536ead6a03f69cd8b37b29ca716c
Cristopher Castro <cristophernic@live.cl>
2018-11-27 16:41:02 -0300

    .

--- application/view/ecografia/PrimerTrimestre.php
+++ application/view/ecografia/PrimerTrimestre.php
@@ -149,7 +149,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-8">
+        <div class="col-4">
             <div class="card">
                 <div class="card-body">
                     <div class="btn-group-vertical" role="group">
