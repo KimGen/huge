bae7bb6245c3fb59a1e22152f679cddd097ebfb1
Cristopher Castro <cristophernic@live.cl>
2018-12-02 20:09:49 -0300

    .

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -2,6 +2,7 @@
     <?php $this->renderFeedbackMessages(); ?>
     <p class="text-center"><small><em>Software obstétrico propiedad intelectual Dr. Rudecindo Lagos, Maternidad Hospital Regional de Temuco, Facultad de Medicina Universidad de La Frontera Temuco - Chile</em></small></p>
     <div class="row">
+    <?php if (Session::userIsLoggedIn() == false) { ?>
         <div class="col-4">
             <div class="card">
                 <div class="card-body">
@@ -25,8 +26,9 @@
                 </div>
             </div>
         </div>
-        <div class="col-5">
-            <p class="text-justify text-azul"><small><em>Sitio web diseñado para profesionales de la salud (médicos) vinculados con la vigilancia del control prenatal.<br>El objetivo de esta aplicación es facilitar a los educandos y profesionales en formación, valoración de datos obstétricos ecográficos relacionados con la vigilancia del crecimiento fetal.<br>Actualmente el software consta de tres módulos, estrechamente interrelacionados entre sí, módulos mediante los cuales se pretende orientar al examinador en los tópicos que a continuación se señalan:<br><br>*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Determinación ecográfica de la edad gestacional<br>*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Valoración de biometrías y crecimiento fetal<br>*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Flujometria Doppler Materno / Fetal básico.</em></small></p>
+        <?php } ?>
+        <div class="col">
+            <p class="text-justify text-azul"><small><em>Sitio web diseñado para profesionales de la salud (médicos) vinculados con la vigilancia del control prenatal.<br>El objetivo de esta aplicación es facilitar a los educandos y profesionales en formación, valoración de datos obstétricos ecográficos relacionados con la vigilancia del crecimiento fetal.<br>Actualmente el software consta de tres módulos, estrechamente relacionados entre sí, módulos mediante los cuales se pretende orientar al examinador en los tópicos que a continuación se señalan:<br><br>*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Determinación ecográfica de la edad gestacional<br>*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Valoración de biometrías y crecimiento fetal<br>*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Flujometria Doppler Materno / Fetal básico.</em></small></p>
         </div>
         <div class="col-3">
             <img src="https://servidor.crecimientofetal.cl/img/feto-computador.png" class="img-fluid">
