809055b2a95ffee823c794bd8bc9e2600dcf21c6
Cristopher Castro <cristophernic@live.cl>
2018-12-02 20:20:06 -0300

    fechas

--- application/view/ecografia/PrimerTrimestre.php
+++ application/view/ecografia/PrimerTrimestre.php
@@ -13,9 +13,9 @@
             <?php } ?>
             <?php if ($this->fur) { ?>
                 <dt class="col-2">FUR:</dt>
-                <dd class="col-4"><?php $date=date_create($this->fur->fur_fecha); echo date_format($date,"d/m/Y"); ?></dd>
+                <dd class="col-2"><?php $date=date_create($this->fur->fur_fecha); echo date_format($date,"d/m/Y"); ?></dd>
                 <dt class="col-2">FPP:</dt>
-                <dd class="col-4"><?php $date2=date_create($this->fur->fpp_fecha); echo date_format($date2,"d/m/Y"); ?></dd>
+                <dd class="col-2"><?php $date2=date_create($this->fur->fpp_fecha); echo date_format($date2,"d/m/Y"); ?></dd>
             <?php } ?>
             </div>
         </div>
