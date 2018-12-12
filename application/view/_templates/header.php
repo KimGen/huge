5be166085eca8b6c7b8a8d924753f9b435318b01
Cristopher Castro <cristophernic@live.cl>
2018-12-12 16:02:55 -0300

    git push

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -47,11 +47,11 @@
         <a class="nav-link" href="#">Ecografía Obstétrica</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="#">Aprendizaje</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
+            Aprendizaje
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="#">Action</a>
