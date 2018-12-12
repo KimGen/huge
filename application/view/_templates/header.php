3a3b1a1eff1dc292a109dcda3aec942458e8a804
Cristopher Castro <cristophernic@live.cl>
2018-12-12 16:07:08 -0300

    git push

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -44,19 +44,19 @@
         <a class="nav-link" href="<?php echo Config::get('URL'); ?>"><i class="fas fa-home"></i></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Ecografía Obstétrica</a>
+        <a class="nav-link" href="#">Pacientes</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Aprendizaje</a>
+        <a class="nav-link" href="#">Ecografía Obstétrica</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Aprendizaje
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">¿Cómo usar la plataforma?</a>
+          <a class="dropdown-item" href="#">¿Cómo calcular EG?</a>
+          <a class="dropdown-item" href="#">¿Cómo realizar una ecografía de primer trimestre?</a>
         </div>
       </li>
       <li class="nav-item">
