bf2fcecfc6bc22ba3fe79b1f790a98d261067d78
Cristopher Castro <cristophernic@live.cl>
2018-12-12 15:49:39 -0300

    menu sombra

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -32,7 +32,7 @@
         </div>
     </div>
 </nav>
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg bg-white shadow">
     <div class="container">
   <a class="navbar-brand d-block d-sm-none" href="#">Menu</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
