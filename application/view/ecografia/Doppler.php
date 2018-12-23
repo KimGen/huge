b3d3472bab2f757feb2365d0e060b5808d66ed75
Cristopher Castro <cristophernic@live.cl>
2018-12-23 02:57:03 -0300

    .

--- application/view/ecografia/Doppler.php
+++ application/view/ecografia/Doppler.php
@@ -159,16 +159,6 @@
                                             <input type="text" class="form-control" id="ecografia.psmacm.mm">
                                         </div>
                                         <div class="form-group col-6">
-                                            <label for="psmacmPct">Pct.</label>
-                                            <div class="progress mt-2 mb-2 mr-sm-2 mb-sm-0" id="psmacmPct">
-                                                <div class="progress-bar bg-light text-primary pivote-cero" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width: 15%">|</div>
-                                                <div class="progress-bar bg-light text-primary pivote-uno" role="progressbar" style="width: 14%" aria-valuemin="0" aria-valuemax="100"></div>
-                                                <div class="progress-bar bg-light text-primary pivote-dos" role="progressbar" style="width: 14%" aria-valuemin="0" aria-valuemax="100"></div>
-                                                <div class="progress-bar bg-light text-primary pivote-centro" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width: 14%">|</div>
-                                                <div class="progress-bar bg-light text-primary pivote-tres" role="progressbar" style="width: 14%" aria-valuemin="0" aria-valuemax="100"></div>
-                                                <div class="progress-bar bg-light text-primary pivote-cuatro" role="progressbar" style="width: 14%" aria-valuemin="0" aria-valuemax="100"></div>
-                                                <div class="progress-bar bg-light text-primary pivote-cien" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width: 15%">|</div>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -670,7 +660,7 @@
         if ( event.which == 13 ) {
             event.preventDefault();
             $("#ecografia\\.psmacm\\.mm").focus();
-            let dv = pctau($("#ecografia\\.eg").val(), $("#ecografia\\.ipacm\\.mm").val());
+            let dv = pctdv($("#ecografia\\.eg").val(), $("#ecografia\\.dv\\.mm").val());
             ajustarProgreso(dv, "dvPct");
         }
     });
@@ -679,8 +669,6 @@
         if ( event.which == 13 ) {
             event.preventDefault();
             $("#button\\.ecografia\\.guardar").focus();
-            let psmacm = pctau($("#ecografia\\.eg").val(), $("#ecografia\\.ipacm\\.mm").val());
-            ajustarProgreso(psmacm, "psmacmPct");
         }
     });
 
@@ -915,6 +903,51 @@
             }
         }
     }
+
+    function pctdv(eg, dv) {
+        var pct5 = [], pct95 = [];
+
+        pct5[0] = 0.32;pct5[1] = 0.32;pct5[2] = 0.32;
+        pct5[3] = 0.32;pct5[4] = 0.32;pct5[5] = 0.32;pct5[6] = 0.31;pct5[7] = 0.31;pct5[8] = 0.31;
+        pct5[9] = 0.3;pct5[10] = 0.29;pct5[11] = 0.28;pct5[12] = 0.28;pct5[13] = 0.27;pct5[14] = 0.26;
+        pct5[15] = 0.25;pct5[16] = 0.24;pct5[17] = 0.23;pct5[18] = 0.22;pct5[19] = 0.21;pct5[20] = 0.2;
+
+        pct95[0] = 0.83;pct95[1] = 0.83;pct95[2] = 0.83;pct95[3] = 0.83;
+        pct95[4] = 0.83;pct95[5] = 0.83;pct95[6] = 0.82;pct95[7] = 0.82;pct95[8] = 0.81;pct95[9] = 0.81;
+        pct95[10] = 0.8;pct95[11] = 0.79;pct95[12] = 0.78;pct95[13] = 0.77;pct95[14] = 0.76;
+        pct95[15] = 0.75;pct95[16] = 0.74;pct95[17] = 0.73;pct95[18] = 0.72;pct95[19] = 0.71;pct95[20] = 0.7;
+
+        dv = dv.toString();
+        dv = dv.replace(',', '.');
+        dv = parseFloat(dv);
+
+        if (eg < 20) {
+            return 0;
+        } 
+        else if (eg > 40)
+        {
+            return 0;
+        } 
+        else {
+            eg = eg - 20;
+            eg = parseInt(eg);
+
+            var uno = pct95[eg] - pct5[eg];
+            var dos = dv - pct5[eg];
+            var resultado = parseInt(90 / (uno) * (dos) + 5);
+
+            if (resultado > 99) {
+                return 0;
+            } 
+            else if (resultado < 1) {
+                return 0;
+            } 
+            else {
+                return resultado;
+            }
+        }
+    }
+
 
 
     function ajustarProgreso(valor, objeto) {
